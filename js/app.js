47de1f611dcffb17d7978f94b5b30230365cfc84
Koustubha068 <KoustuK@gmail.com>
2024-05-28 17:55:32 -0400

    created if else statement for code and an arry for add a to do item

--- js/app.js
+++ js/app.js
@@ -11,3 +11,19 @@ const LINE_THROUGH = "lineThrough";
 let LIST, id;
 
 let data = localStorage.getItem("TODO");
+
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length; // set the id to the last one in the list
+    loadList(LIST); // load the list to the user interface
+}else{
+    // if data isn't empty
+    LIST = [];
+    id = 0;
+}
+
+function loadList(array){
+    array.forEach(function(item){
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
