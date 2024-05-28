5d5cb9ab1bc7737cac8ea790e0180ce3bf80c3ef
Koustubha068 <KoustuK@gmail.com>
2024-05-28 17:41:00 -0400

    created variables such as clear,list,input, etc

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 // CODE EXPLAINED channel
+
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+//vars
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+let LIST, id;
+
+let data = localStorage.getItem("TODO");
